ca1b658941a49ba62a7f10bd91db5539ee194c18
Marcus <marcuspolo77@Marcuss-MacBook-Pro.local>
2017-03-16 21:04:56 -0700

    deletes

---
+++ cgi/cgi-bin/cgi_1.py
@@ -0,0 +1,5 @@
+/Users/marcuspolo77/Documents/pyclas/InternetProgramming/cgi/cgi_1.py
+import cgi
+
+
+cgi.test()
---
+++ djangoenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/marcuspolo77/Documents/pyclas/InternetProgramming/djangoenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
